e5f5a1e888040afbc363c39f56c6accc9efd91ad
manish <mvsmanish07@gmail.com>
2024-06-13 23:02:37 +0530

    added feature to add hotel

---
+++ frontend/src/config/hotel-options-config.ts
@@ -0,0 +1,24 @@
+export const hotelTypes=[
+    "Budget",
+    "Luxury",
+    "Family",
+    "Romantic",
+    "Beach Nearby",
+    "Hiking Resort",
+    "Cabin",
+    "City Centre",
+    "Boutique",
+    "Pet Friendly",
+];
+
+export const hotelFacilities=[
+    "Free Wifi",
+    "Parking",
+    "Family Rooms",
+    "Spa",
+    "Gym Facilities",
+    "Pool Available",
+    "Food Court Available",
+    "Non-Smoking Rooms",
+    
+];
