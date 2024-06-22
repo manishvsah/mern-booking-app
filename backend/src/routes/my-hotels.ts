bef39fd9375e11c223d06648f9316845c908f9bd
manish <mvsmanish07@gmail.com>
2024-06-23 03:40:08 +0530

    added edit hotel

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -12,66 +12,121 @@ const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 5 * 1024 * 1024, //5 mb
+    fileSize: 5 * 1024 * 1024, // 5MB
   },
 });
 
-//api/my-hotels
 router.post(
   "/",
-  verifyToken, 
+  verifyToken,
   [
-    body("name").notEmpty().withMessage('Name is required'),
-    body("city").notEmpty().withMessage('city is required'),
-    body("country").notEmpty().withMessage('country is required'),
-    body("description").notEmpty().withMessage('Description is required'),
-    body("type").notEmpty().withMessage('Hotel type is required'),
+    body("name").notEmpty().withMessage("Name is required"),
+    body("city").notEmpty().withMessage("City is required"),
+    body("country").notEmpty().withMessage("Country is required"),
+    body("description").notEmpty().withMessage("Description is required"),
+    body("type").notEmpty().withMessage("Hotel type is required"),
     body("pricePerNight")
-        .notEmpty()
-        .isNumeric()
-        .withMessage('Price per night is requied and it must be a number'),
+      .notEmpty()
+      .isNumeric()
+      .withMessage("Price per night is required and must be a number"),
     body("facilities")
-        .notEmpty()
-        .isArray().
-        withMessage('Name is required'),
+      .notEmpty()
+      .isArray()
+      .withMessage("Facilities are required"),
   ],
   upload.array("imageFiles", 6),
   async (req: Request, res: Response) => {
-    try{
-        const imageFiles= req.files as Express.Multer.File[];
-        const newHotel : HotelType = req.body;
-        
-        const uploadPromises=imageFiles.map(async(image)=>{
-            const b64=Buffer.from(image.buffer).toString("base64");
-            let dataURI="data:"+image.mimetype+";base64,"+ b64;
-            const res = await cloudinary.v2.uploader.upload(dataURI);
-            return res.url;
-        });
+    try {
+      const imageFiles = req.files as Express.Multer.File[];
+      const newHotel: HotelType = req.body;
 
-        const imageUrls=await Promise.all(uploadPromises);
-        newHotel.imageUrls=imageUrls;
-        newHotel.lastUpdated=new Date();
-        newHotel.userId= req.userId;
+      const imageUrls = await uploadImages(imageFiles);
 
-        const hotel=new Hotel(newHotel);
-        await hotel.save();
+      newHotel.imageUrls = imageUrls;
+      newHotel.lastUpdated = new Date();
+      newHotel.userId = req.userId;
 
-        res.status(201).send(hotel);
+      const hotel = new Hotel(newHotel);
+      await hotel.save();
 
-    }catch(e){
-        console.log("Error creating hotel: ",e);
-        res.status(500).json({message:"Something went wrong"});
+      res.status(201).send(hotel);
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ message: "Something went wrong" });
     }
   }
 );
 
-router.get("/",verifyToken, async(req:Request, res:Response)=>{
-  try{
-    const hotels=await Hotel.find({userId:req.userId});
+router.get("/", verifyToken, async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotel.find({ userId: req.userId });
     res.json(hotels);
-  }catch(error){
-    res.status(500).json({message:"Error fetching hotels"});
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching hotels" });
   }
 });
+
+router.get("/:id", verifyToken, async (req: Request, res: Response) => {
+  const id = req.params.id.toString();
+  try {
+    const hotel = await Hotel.findOne({
+      _id: id,
+      userId: req.userId,
+    });
+    res.json(hotel);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+});
+
+router.put(
+  "/:hotelId",
+  verifyToken,
+  upload.array("imageFiles"),
+  async (req: Request, res: Response) => {
+    try {
+      const updatedHotel: HotelType = req.body;
+      updatedHotel.lastUpdated = new Date();
+
+      const hotel = await Hotel.findOneAndUpdate(
+        {
+          _id: req.params.hotelId,
+          userId: req.userId,
+        },
+        updatedHotel,
+        { new: true }
+      );
+
+      if (!hotel) {
+        return res.status(404).json({ message: "Hotel not found" });
+      }
+
+      const files = req.files as Express.Multer.File[];
+      const updatedImageUrls = await uploadImages(files);
+
+      hotel.imageUrls = [
+        ...updatedImageUrls,
+        ...(updatedHotel.imageUrls || []),
+      ];
+
+      await hotel.save();
+      res.status(201).json(hotel);
+    } catch (error) {
+      res.status(500).json({ message: "Something went throw" });
+    }
+  }
+);
+
+async function uploadImages(imageFiles: Express.Multer.File[]) {
+  const uploadPromises = imageFiles.map(async (image) => {
+    const b64 = Buffer.from(image.buffer).toString("base64");
+    let dataURI = "data:" + image.mimetype + ";base64," + b64;
+    const res = await cloudinary.v2.uploader.upload(dataURI);
+    return res.url;
+  });
+
+  const imageUrls = await Promise.all(uploadPromises);
+  return imageUrls;
+}
 
 export default router;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
+import EditHotel from "./pages/EditHotel";
 
 const App=()=>{
   const {isLoggedIn}=useAppContext();
@@ -48,22 +49,32 @@ const App=()=>{
             </Layout>
            }/>
 
-           {isLoggedIn && (
-           <>
-              <Route 
-                path="/add-hotel" 
-                element={
-                  <Layout>
-                    <AddHotel/>
-                  </Layout>
+{isLoggedIn && (
+          <>
+
+
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
               }
-              />
-              <Route 
-                path="/my-hotels" 
-                element={
-                  <Layout>
-                    <MyHotels/>
-                  </Layout>
+            />
+            <Route
+              path="/edit-hotel/:hotelId"
+              element={
+                <Layout>
+                  <EditHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-hotels"
+              element={
+                <Layout>
+                  <MyHotels />
+                </Layout>
               }
               />
            </>
